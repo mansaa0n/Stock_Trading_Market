8b64c004c5b401ea7fe43f71c80bab7ddbc946d7
Ahmed <mansaa0n@gmail.com>
2016-11-17 15:18:08 -0800

    update

---
+++ automated stock trading/stock.h
@@ -0,0 +1,23 @@
+//
+//  stock.h
+//  automated stock trading
+//
+//  Created by Ahmed Almansor on 11/17/16.
+//  Copyright © 2016 Fresno State Electric & Computer Eng. All rights reserved.
+//
+
+#include <vector>
+using namespace std;
+
+#ifndef stock_h
+#define stock_h
+
+class stock_trading {
+    public:
+    vector <int> PurchaseStocks;
+
+    //     saprate files
+};
+
+
+#endif /* stock_h */
